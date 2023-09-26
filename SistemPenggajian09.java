db63d116d3f574ec980ca8854a36d13ca0dddc61
Tama Dioo <tamadio91@gmail.com>
2023-09-26 20:49:22 +0700

    Sistem penggajian

--- SistemPenggajian09.java
+++ SistemPenggajian09.java
@@ -1,3 +1,29 @@
-public class gaji09 {
-    
+import java.util.Scanner;
+public class SistemPenggajian09 {
+    public class gaji {
+        public static void main(String[] args) throws Exception {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.print("Masukkan nama: ");
+            String nama = scan.nextLine();
+            System.out.print("Masukkan jumlah hari kerja: ");
+            int hariKerja = scan.nextInt();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            int jamLembur = scan.nextInt();
+
+            int gajiPokok = 30000;
+            int jumlahGajiPokok = gajiPokok * hariKerja;
+            int gajiLembur = 6000;
+            int jumlahGajiLembur = gajiLembur * jamLembur;
+
+            int jumlahGajiBulanan = jumlahGajiPokok + jumlahGajiLembur;
+
+            System.out.println("----------------------------------");
+            System.out.println("Nama : " + nama);
+            System.out.println("Gaji pokok : " + jumlahGajiPokok);
+            System.out.println("Gaji lembur : " + jumlahGajiLembur);
+            System.out.println("Jumlah gaji bulanan : " + jumlahGajiBulanan);
+            System.out.println("==================================");   
+        }
+    }
 }
